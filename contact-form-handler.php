48e8a623e018df13d5f9cfa3f5d3778c0365f527
RobertoBenedit <roberto.benedit.contador@gmail.com>
2022-06-28 22:47:30 -0300

    first commit

---
+++ contact-form-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name =  $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = '...';
+
+$email_subject = "llego una nueva solicitud";
+
+$email_body = "User Name: $name.\n" .
+    "User Email: $visitor_email.\n" .
+    "User Message: $message.\n";
+
+
+$to = "robertobenedit@outlook.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: index.html");
+
+?>
